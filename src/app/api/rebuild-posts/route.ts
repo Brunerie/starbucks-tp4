4c538d5e5a950866e7b8e038c08d6aa61c7cc9dc
Mathieu BRUNERIE <mb936701@iut21.u-bourgogne.fr>
2024-02-05 16:43:03 +0100

    question 3.6

---
+++ src/app/api/rebuild-posts/route.ts
@@ -0,0 +1,18 @@
+import {NextRequest, NextResponse} from "next/server";
+export async function POST(request: NextRequest) {
+    const apiKey = process.env.SUPABASE_WEBHOOK_KEY;
+
+    if (!request.headers.has('API-Key')) {
+        return NextResponse.json({message : "\"API-Key\" header is missing"}, {status: 401});
+    }
+    if (request.headers.get('API-Key') !== apiKey) {
+        return NextResponse.json({message : "API Key is not valid"}, {status: 403});
+    }
+
+    console.log(request.headers);
+
+    return NextResponse.json({
+        "revalidated": true,
+        "date": "2023-08-16T14:22:44.119Z"
+    });
+}
